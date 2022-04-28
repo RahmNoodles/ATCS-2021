0f3580e750b8fbec933c2af5b9ed58525d732c87
Zain Rahman <zain.rahman@menloschool.org>
2022-04-28 11:56:47 -0700

    Main sprite addition

--- FinalProject/main.py
+++ FinalProject/main.py
@@ -3,26 +3,66 @@ import arcade
 
 # Constants
 SCREEN_WIDTH = 1000
-SCREEN_HEIGHT = 500
+SCREEN_HEIGHT = 800
 SCREEN_TITLE = "Pong"
 RADIUS = 10
+SCALING = 2.0
+PITCH_SCALING = 2.78
 
-# Open the window
-arcade.open_window(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+class Pong(arcade.Window):
+    """Space Shooter side scroller game
+    Player starts on the left, enemies appear on the right
+    Player can move anywhere, but not off screen
+    Enemies fly to the left at variable speed
+    Collisions end the game
+    """
 
-# Set the background color
-arcade.set_background_color(arcade.color.BLACK)
+    def __init__(self, width, height, title):
+        """Initialize the game
+        """
+        super().__init__(width, height, title)
+        arcade.set_background_color(arcade.color.BLUE)
 
-# Clear the screen and start drawing
-arcade.start_render()
+        # Set up the empty sprite lists
+        self.players_list = arcade.SpriteList()
+        self.ball_list = arcade.SpriteList()
+        self.ball_list = arcade.SpriteList()
+        self.all_sprites = arcade.SpriteList()
 
-# Draw a blue circle
-arcade.draw_circle_filled(
-    SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, RADIUS, arcade.color.WHITE
-)
+        self.setup()
 
-# Finish drawing
-arcade.finish_render()
+    def setup(self):
+        """Get the game ready to play
+        """
 
-# Display everything
-arcade.run()
+        # Set the background color
+        #self.player = arcade.Sprite("data/Pitch.png", SCALING)
+        #arcade.set_background_color(self.player)
+
+        # Set up the player
+        self.player = arcade.Sprite("data/Neymar.tiff", SCALING)
+        self.player.center_y = self.height / 2
+        self.player.left = 0
+        self.all_sprites.append(self.player)
+
+        self.player = arcade.Sprite("data/Messi.tiff", SCALING)
+        self.player.center_y = self.height / 2
+        self.player.left = SCREEN_WIDTH - 217
+        self.all_sprites.append(self.player)
+
+    def on_draw(self):
+        """Called whenever you need to draw your window
+        """
+
+        # Clear the screen and start drawing
+        arcade.start_render()
+        #Draw sprites
+        self.all_sprites.draw()
+        # Draw a blue circle
+        arcade.draw_circle_filled(
+            SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, RADIUS, arcade.color.BLUE
+        )
+
+if __name__ == '__main__':
+    game = Pong(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+    arcade.run()
